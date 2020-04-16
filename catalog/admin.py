78ad4089ae9871967576caa81b5b0032f72d4222
root <root@vmi370354.contaboserver.net>
2020-04-16 18:01:24 +0200

    db3

--- catalog/admin.py
+++ catalog/admin.py
@@ -7,6 +7,28 @@ class ProductMultiImage(admin.TabularInline):
     model = MultiImage
     fields = ['image',]
 
+class ProductMultiSize(admin.TabularInline):
+    model = Size
+    fields = ['name',]
+
+class ProductMultiPromoCode(admin.TabularInline):
+    model = PromoCode
+    fields = ['name', 'code', ]
+	
+class Kredit_18Admin(admin.StackedInline):
+    #extra = 1
+	
+	model = Kredit_18_ay
+	fields = ['faiz', 'odenis',]
+	show_change_link = True
+	extra = 1
+	readonly_fields = ['odenis',]
+	def get_max_num(self, request, obj=None, **kwargs):
+		max_num = 1
+		if obj and obj.kredit:
+			return max_num 
+		return max_num
+
 def make_payed(modeladmin, request, queryset):
     queryset.update(status='Odenilib')
 make_payed.short_description = "Odenilmis kimi qeyd et"
@@ -15,20 +37,30 @@ class OrderAdmin(admin.ModelAdmin):
 	list_filter = ['status']
 	actions = [make_payed]
 
-
 class ProductAdmin(admin.ModelAdmin):
-	inlines = [ProductMultiImage,]
-	list_display = ("category", "name", "active", "stock", "price", "sale", "dicount",)
+	inlines = [ProductMultiImage, ProductMultiPromoCode,Kredit_18Admin,ProductMultiSize]
+	extra = 1
+	list_display = ("category", "name", "active", "stock", "price", "sale", "dicount","prome_code_in","kredit_18",)
 	model = Product
+	#extra = 2
+	list_filter = ('active','codes__name','company__name',)
+	readonly_fields = ['slug','month_6', 'month_12',]
+	#fields = ['kredit',]
+	
 
 
 admin.site.register(MultiImage)
 admin.site.register(Fovarite)
 admin.site.register(Color)
+admin.site.register(PromoCode)
+
+admin.site.register(CompanyPromoCode)
 
 admin.site.register(Category)
 admin.site.register(Brand)
 admin.site.register(SubCategory)
+admin.site.register(Kredit_18_ay)
+
 
 admin.site.register(Product, ProductAdmin)
 admin.site.register(CartItem)
--- catalog/models.py
+++ catalog/models.py
@@ -12,7 +12,7 @@ from PIL import Image
 import os
 import glob
 #from simple_history.models import HistoricalRecords
-
+import math
 # Create your models here.
 
 class Brand(models.Model):
@@ -59,17 +59,22 @@ def gen_slug(s):
 
     return new_slug + '_' + str(int(time()))
 
+def gen_random_promo():
+    #new_slug = slugify(s, allow_unicode=True)
+
+    return str(int(time()))
 
 class Color(models.Model):
     name = models.CharField(max_length=100)
     def __str__(self):
         return self.name
+
+
     
 
 
 class Product(models.Model):
     category = models.ForeignKey(SubCategory,on_delete=models.CASCADE, verbose_name='Kategoriya')
-#    colors = models.ForeignKey(Color,on_delete=models.CASCADE, verbose_name='Rengi', null=True, blank=True)
     colors = models.ManyToManyField(Color, verbose_name='Rengi', blank=True)
     brand = models.ForeignKey(Brand, on_delete=models.CASCADE, verbose_name='Brand')
     name = models.CharField(max_length=1000, verbose_name='Mehsulun Adi')
@@ -86,19 +91,35 @@ class Product(models.Model):
     data = models.DateField(auto_now_add=True)
     stock = models.BooleanField(default=True)
     active = models.BooleanField(default=True)
-    promo_kod = models.CharField(max_length=200, blank=True)
+    month_6 = models.CharField(max_length=200,blank=True, verbose_name='Kredit 6 ay ucun ayliq odenis')
+    month_12 = models.CharField(max_length=200,blank=True, verbose_name='Kredit 12 ay ucun ayliq odenis')
+
+    #promo_kod = models.CharField(max_length=200, blank=True)
     slug = models.SlugField(max_length=200, blank=True)
     material = models.CharField(max_length=200, blank=True, verbose_name='Material')
-    olcu = models.CharField(max_length=200, blank=True, verbose_name='Olculer')
+   # olcu = models.CharField(max_length=200, blank=True, verbose_name='Olculer')
     #history = HistoricalRecords()
     #fovarite = models.BooleanField(default=False)
+    def prome_code_in(self):
+       return self.codes.all().count()
 
+    def kredit_18(self):
+       x = self.kredit.filter(product=self.id)
+       for i in x:
+           x = i.odenis
+           return x
 
     def save (self, *args, **kwargs):
         if not self.id:
             self.slug = gen_slug(self.name)
         if not self.dicount:
             self.dicount = self.price - (self.price * self.sale / 100)
+        if not self.month_6:
+            self.month_6 = math.ceil(self.price / 6)
+        if not self.month_12:
+            self.month_12 = math.ceil(self.price / 12)
+       
+        #if sel
         super().save(*args, **kwargs)
 
     def __str__(self):
@@ -120,6 +141,57 @@ class CartItem(models.Model):
     def  __str__(self):
         return "Cart item for product {0}".format(self.product.title)
 
+class CompanyPromoCode(models.Model):
+    name = models.CharField(max_length=400, verbose_name='Kampaniya Adi')
+    faiz = models.IntegerField(verbose_name='Endirim Faiz Derecesi')
+    products = models.ManyToManyField(Product, verbose_name='Mehsullar', related_name='company')
+    code = models.CharField(max_length=300, verbose_name='Code')
+    limit = models.IntegerField(verbose_name='Maksimum istifade Dayi - Limit')
+    count = models.IntegerField(verbose_name='Promocoddan istifade olunub', blank=True)
+    def __str__(self):
+        return self.name
+
+    
+class Kredit_18_ay(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='kredit')
+    faiz = models.IntegerField(verbose_name='18 Ay ucun faiz derecesi', blank=True)
+    odenis = models.CharField(max_length=100 , blank=True, verbose_name='Ayliq odenis')
+    
+    def save (self, *args, **kwargs):
+       
+        if not self.odenis:
+            x = self.product.price + (self.product.price * self.faiz / 100 )
+            self.odenis = math.ceil(x / 18)
+        #if sel
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return str(self.product.name)
+    
+    
+    
+    
+class Size(models.Model):
+    products = models.ForeignKey(Product, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, verbose_name='Olculer')
+
+    def __str__(self):
+        return self.name
+
+class PromoCode(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='codes')
+    name = models.IntegerField(verbose_name='Promocodun Faiz Derecesi')
+    code = models.CharField(max_length=300, verbose_name='Code', blank=True)
+    def save (self, *args, **kwargs):
+       
+        if not self.code:
+            #x = self.product.price + (self.product.price * self.faiz / 100 )
+            self.code = gen_random_promo()
+        super().save(*args, **kwargs)
+
+
+    def __str__(self):
+        return self.product.name
 
 class Cart(models.Model):
 
@@ -189,3 +261,4 @@ class Fovarite(models.Model):
 
   
     
+
