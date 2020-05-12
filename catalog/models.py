d2cb4623822bda25ead985b1733725f703d36691
root <root@vmi370354.contaboserver.net>
2020-05-12 10:33:25 +0200

    bb

--- catalog/models.py
+++ catalog/models.py
@@ -27,7 +27,7 @@ from django.contrib.auth.models import (
 def compress(image):
     im = Image.open(image)
     im_io = BytesIO() 
-    im.save(im_io, 'JPEG', quality=60) 
+    im.save(im_io, 'JPEG', quality=90) 
     new_image = File(im_io, name=image.name)
     return new_image
 
